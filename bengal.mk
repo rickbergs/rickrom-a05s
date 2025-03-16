f9474f3e1d8a418f593065d5ac694f3c1f7dbe32
SavedByLight <mrfluffyoven@gmail.com>
2025-03-16 08:45:12 +0000

    enable overlays in tree

--- bengal.mk
+++ bengal.mk
@@ -222,9 +222,9 @@ PRODUCT_PACKAGES += \
     Tag
 
 # Overlay
-#DEVICE_PACKAGE_OVERLAYS += \
+DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-aosp
+    #$(LOCAL_PATH)/overlay-aosp
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
