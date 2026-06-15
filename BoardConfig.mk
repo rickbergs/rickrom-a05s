c2f9730e86834a2cd0304fa37fdfe2be61da1057
Richard Bergamaschi <richardbergamaschi65@gmail.com>
2026-06-15 16:52:04 -0300

    a05s: fix dynamic partition layout for A057M

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -8,3 +8,23 @@ BOARD_PREBUILT_DTBOIMAGE := $(DEVICE_PATH)/prebuilts/dtbo.img
 
 # Inherit from common tree
 include device/samsung/bengal-common/BoardConfigCommon.mk
+
+# BEGIN SM-A057M PARTITION OVERRIDES
+# Values obtained from the stock A057MUBSCDYJ2 LP metadata.
+# These override the provisional values from bengal-common.
+
+BOARD_SUPER_PARTITION_SIZE := 9017753600
+
+BOARD_SUPER_PARTITION_GROUPS := samsung_dynamic_partitions
+
+BOARD_SAMSUNG_DYNAMIC_PARTITIONS_SIZE := 9013559296
+
+BOARD_SAMSUNG_DYNAMIC_PARTITIONS_PARTITION_LIST := \
+    system \
+    system_ext \
+    product \
+    vendor \
+    odm \
+    system_dlkm \
+    vendor_dlkm
+# END SM-A057M PARTITION OVERRIDES
